0e3f1ada24de07e63f88796c2d514ad38e43c23c
Tatsuya28 <ioiodauphin880@gmail.com>
2022-11-18 23:42:15 +0100

    First commit: creation of the repo

---
+++ database.c
@@ -0,0 +1,17 @@
+#include "database.h"
+
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <stdio.h>
+
+#include "utils.h"
+
+void create_db_directory(char *name) {
+    if (!directory_exists(name)) {
+        mkdir(name, S_IRWXU);
+    }
+}
+
+void recursive_rmdir(char *dirname) {
+}
---
+++ database.h
@@ -0,0 +1,7 @@
+#ifndef _DATABASE_H
+#define _DATABASE_H
+
+void create_db_directory(char *name);
+void recursive_rmdir(char *dirname);
+
+#endif //_DATABASE_H
