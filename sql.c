ee4279230780ec2a6c5266f939b884abe41733ee
Tatsuya28 <ioiodauphin880@gmail.com>
2022-11-19 00:13:32 +0100

    Implementation of get_sep_space and get_sep_space_and_char

--- sql.c
+++ sql.c
@@ -4,12 +4,48 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+/**
+ * Checks the presence of a sequence of one to an indeterminate number of spaces from the sql position.
+ * @param sql the string to check
+ * @return a pointer to the first non-space encountered
+ */
 char *get_sep_space(char *sql) {
-    return sql;
+    if (!sql)
+        return NULL;
+
+    char* copy_sql = sql;
+
+    while (copy_sql[0] == ' ')
+        copy_sql++;
+
+    return copy_sql;
 }
 
+
+/**
+ * Checks for a sequence of 0 or more spaces, then for the character c once, then for 0 or more spaces.
+ * @param sql the string to check
+ * @param c the unique character
+ * @return a pointer to the character following this sequence
+ */
 char *get_sep_space_and_char(char *sql, char c) {
-    return sql;
+    if (!sql)
+        return NULL;
+
+    char* copy_sql = sql;
+    bool separator_found = false;
+
+    while (copy_sql[0] == ' ' || copy_sql[0] == c) {
+        if (copy_sql[0] == c)
+            separator_found = true;
+
+        copy_sql++;
+    }
+
+    if (!separator_found)
+        return NULL;
+
+    return get_sep_space(copy_sql);
 }
 
 char *get_keyword(char *sql, char *keyword) {
